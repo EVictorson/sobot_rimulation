13a6ae9a8a859480655a45b20abb47b4c803bfd4
evictorson <vier0801@gmail.com>
2019-11-08 00:02:41 -0600

    add state derivatives

--- sobot_rimulation/TrajectoryGen.py
+++ sobot_rimulation/TrajectoryGen.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 """ Simple class to generate robot manipulator joint space trajectories.
 
+This class only performs direct kinematics.  If a task space goal is desired
+you must first convert your task space goal to a joint space goal via the associated
+inverse kinematics module.
 Methods currently employed are cubic polynomial and quintic polynomial.
 The CubicPoly method takes in angular position and angular velocity.
 The QuinticPoly method takes in angular position, velocity and acceleration.
@@ -45,6 +48,8 @@ class TrajectoryGen:
         self.rate = rate
         self.A = []
         self.q = []
+        self.qdot = []
+        self.qddot = []
 
     def return_trajectory(self):
         """ Return generated trajectory. """
@@ -86,12 +91,18 @@ class CubicPoly(TrajectoryGen):
         # for each joint calculate list of positions
         for joint in range(len(self.q0)):
             q = []
+            qdot = []
+            qddot = []
             for i in range(num_points):
                 t = (float(i) / self.rate)
                 q.append(A[joint][3] * t**3 + A[joint][2] * t**2 + A[joint][1]*t + A[joint][0])
+                qdot.append(3*A[joint][3] * t**2 + 2*A[joint][2] * t + A[joint][1])
+                qddot.append(6*A[joint][3] * t + 2*A[joint][2])
 
             # appen list of joint position i to q
             self.q.append(q)
+            self.qdot.append(qdot)
+            self.qddot.append(qddot)
 
 
 
@@ -122,6 +133,8 @@ class QuinticPoly(TrajectoryGen):
         self.rate = rate
         self.A = []
         self.q = []
+        self.qdot = []
+        self.qddot = []
 
 
     def calc_coeffs(self):
@@ -159,12 +172,18 @@ class QuinticPoly(TrajectoryGen):
         # for each joint calculate list of positions
         for joint in range(len(self.q0)):
             q = []
+            qdot = []
+            qddot = []
             for i in range(num_points):
                 t = float(i) / self.rate
-                q.append(A[joint,5] * t**5 + A[joint,4] * t**4 + A [joint,3] * t**3 + A[joint,2] * t**2 + A[joint,1] * t + A[joint,0])
+                q.append(A[joint][5] * t**5 + A[joint][4] * t**4 + A[joint][3] * t**3 + A[joint][2] * t**2 + A[joint][1] * t + A[joint][0])
+                qdot.append(5*A[joint][5] * t**4 + 4*A[joint][4] * t**3 + 3*A[joint][3] * t**2 + 2*A[joint][2] * t + A[joint][1])
+                qddot.append(20*A[joint][5] * t**3 + 12*A[joint][4] * t**2 + 6*A[joint][3] * t + 2*A[joint][2])
 
             # appen list of joint position i to q
             self.q.append(q)
+            self.qdot.append(qdot)
+            self.qddot.append(qddot)
 
 
 # if main do test
@@ -180,19 +199,45 @@ if __name__ == '__main__':
     TGen = CubicPoly(q0,q0dot,qf,qfdot,tf,traj_rate)
     TGen.run()
     traj = TGen.q
+    traj_dot = TGen.qdot
+    traj_ddot = TGen.qddot
 
     t = np.linspace(0, tf, traj_rate*tf)
     theta1 = traj[0]
     theta2 = traj[1]
+    omega1 = traj_dot[0]
+    omega2 = traj_dot[1]
+    alpha1 = traj_ddot[0]
+    alpha2 = traj_ddot[1]
+
 
     print(len(t))
     print(len(theta1))
 
-    plt.plot(t, theta1, 'b', label='Theta1')
-    plt.plot(t, theta2, 'r', label='Theta2')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Joint Angle (rad)')
-    plt.title('Robot Manipulator Joint Angles vs Time')
-    plt.legend()
-    plt.grid()
+
+    f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=False)
+    ax1.plot(t,theta1, color='b', label='Theta1')
+    ax1.plot(t,theta2, color='r', label='Theta2')
+    ax1.grid()
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Angular Position (rad)')
+    ax1.set_title('Joint Position vs Time')
+    ax1.legend(loc='upper left')
+
+    ax2.plot(t,omega1, color='b', label='Omega1')
+    ax2.plot(t,omega2, color='r', label='Omega2')
+    ax2.grid()
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Angular Velocity (rad/s)')
+    ax2.set_title('Joint Velocity vs Time')
+    ax2.legend(loc='upper left')
+
+    ax3.plot(t,alpha1, color='b', label='Alpha1')
+    ax3.plot(t,alpha2, color='r', label='Alpha2')
+    ax3.grid()
+    ax3.set_xlabel('Time (s)')
+    ax3.set_ylabel('Angular Acceleration (rad/s/s)')
+    ax3.set_title('Joint Acceleration vs Time')
+    ax3.legend(loc='upper left')
+
     plt.show()
